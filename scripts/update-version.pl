dc41821de94a0cbd0cb0f9621d13995b88583461
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-01-11 16:04:30 +0800

    :ambulance: Remove ^v from $version

--- scripts/update-version.pl
+++ scripts/update-version.pl
@@ -4,6 +4,7 @@ BEGIN {
     $version =
 `curl -s https://api.github.com/repos/xmake-io/xmake/releases/latest | jq -r .tag_name`;
     chomp $version;
+    $version =~ s/^v//;
 }
 
 s/(?<=version = ")[^"]+/$version/;
