dc807b2956cce25e72b99230591d6f734316d14f
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-01-14 00:46:47 +0800

    :art: Format js/json by biome

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
-const os = require('os');
-const fs = require('fs');
-const {resolve} = require('path');
-const {Readable} = require('stream');
-const {spawnSync} = require('child_process');
+const os = require('node:os');
+const fs = require('node:fs');
+const { resolve } = require('node:path');
+const { Readable } = require('node:stream');
+const { spawnSync } = require('node:child_process');
 
 const tar = require('tar');
 
-const wd = resolve(__dirname, "..")
-const version = JSON.parse(fs.readFileSync(resolve(wd, "package.json"))).version;
-const cwd = resolve(wd, `xmake-${version}`)
-const file = `${cwd}.tar.gz`
+const wd = resolve(__dirname, '..');
+const version = JSON.parse(
+  fs.readFileSync(resolve(wd, 'package.json')),
+).version;
+const cwd = resolve(wd, `xmake-${version}`);
+const file = `${cwd}.tar.gz`;
 const url = `https://github.com/xmake-io/xmake/releases/download/v${version}/xmake-v${version}.tar.gz`;
 const prefix = resolve(wd, `prebuilds/xmake-${os.platform()}-${os.arch()}`);
 
@@ -30,19 +32,16 @@ async function download(url, file) {
 
 function run(...args) {
   const cmd = spawnSync(...args);
-  console.log(cmd.stdout.toString())
-  console.error(cmd.stderr.toString())
+  console.log(cmd.stdout.toString());
+  console.error(cmd.stderr.toString());
 }
 
 (async () => {
-  if (!fs.existsSync(file))
-    await download(url, file)
-  if (!fs.existsSync(cwd))
-    await tar.x({file: file, C: '.'})
+  if (!fs.existsSync(file)) await download(url, file);
+  if (!fs.existsSync(cwd)) await tar.x({ file: file, C: '.' });
   if (!fs.existsSync(resolve(cwd, 'makefile')))
-    run("sh", ['configure', `--prefix=${prefix}`], {cwd: cwd})
-  if (!fs.existsSync(resolve(cwd, 'build')))
-    run("make", {cwd: cwd})
+    run('sh', ['configure', `--prefix=${prefix}`], { cwd: cwd });
+  if (!fs.existsSync(resolve(cwd, 'build'))) run('make', { cwd: cwd });
   if (!fs.existsSync(resolve(prefix, 'bin', 'xmake')))
-    run("make", ["install"], {cwd: cwd})
-})()
+    run('make', ['install'], { cwd: cwd });
+})();
