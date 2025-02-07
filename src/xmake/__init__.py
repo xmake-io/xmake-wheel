6f03802b05380ee676637c91cbd3db904909db3a
Freed-Wu <32936898+Freed-Wu@users.noreply.github.com>
2025-02-07 18:22:03 +0000

    Apply automatic changes

--- src/xmake/__init__.py
+++ src/xmake/__init__.py
@@ -2,4 +2,4 @@ r"""Provide ``__version__`` for
 `importlib.metadata.version() <https://docs.python.org/3/library/importlib.metadata.html#distribution-versions>`_.
 """
 
-__version__ = "2.9.7"
+__version__ = "2.9.8"
