f939a997fe35e03618e336b1294485ebbcf4d514
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-05-27 02:38:38 +0800

    :green_heart: Fix CI for updating version

--- scripts/update-version.pl
+++ scripts/update-version.pl
@@ -5,9 +5,12 @@ BEGIN {
 `curl -s https://api.github.com/repos/xmake-io/xmake/releases/latest | jq -r .tag_name`;
     chomp $version;
     $version =~ s/^v//;
+    $sha =
+`curl -s https://github.com/xmake-io/xmake/releases/download/$version/xmake-$version.tar.gz | sha256sum`;
+    chomp $sha;
 }
 
 s/(?<=version = ")[^"]+/$version/;
-s/(?<=__version__ = ")[^"]+/$version/;
 s/(?<=  "version": ")[^"]+/$version/;
-s/(?<=ref: )[^"]+/$version/;
+s/(?<=    add_versions("v)[^"]+/$version/;
+s/(?<=    add_versions("v$version", ")[^"]+/$sha/;
