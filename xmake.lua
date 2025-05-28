19c9fa987c879e709ae935cbeede78e013552faf
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-05-29 00:10:59 +0800

    :bug: Add scripts for Windows

--- xmake.lua
+++ xmake.lua
@@ -34,8 +34,9 @@ do
         function(target)
             local prefix = target:pkg("xmake"):installdir()
             if target:is_plat("windows") then
-                target:add("installfiles", path.join(prefix, "xmake.exe"), { prefixdir = "$(xmake-scripts)" })
-                target:add("installfiles", path.join(prefix, "(*/**)"), { prefixdir = "$(xmake-data)" })
+                target:add("installfiles", "scripts/xmake.cmd", { prefixdir = "$(xmake-scripts)" })
+                target:add("installfiles", "scripts/xmake.ps1", { prefixdir = "$(xmake-scripts)" })
+                target:add("installfiles", path.join(prefix, "*"), { prefixdir = "$(xmake-data)/share" })
             else
                 target:add("installfiles", path.join(prefix, "bin/*"), { prefixdir = "$(xmake-scripts)" })
                 target:add("installfiles", path.join(prefix, "(share/**)"), { prefixdir = "$(xmake-data)" })
