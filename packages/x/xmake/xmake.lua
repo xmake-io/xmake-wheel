a9374f0ef58b067e0f16bc9408409f39d5493690
Freed-Wu <32936898+Freed-Wu@users.noreply.github.com>
2025-07-18 18:28:26 +0000

    Apply automatic changes

--- packages/x/xmake/xmake.lua
+++ packages/x/xmake/xmake.lua
@@ -6,7 +6,7 @@ do
     set_description("xmake - A cross-platform build utility based on Lua")
 
     set_urls("https://github.com/xmake-io/xmake/releases/download/$(version)/xmake-$(version).tar.gz")
-    add_versions("v3.0.0", "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5")
+    add_versions("v3.0.1", "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5")
 
     on_install(function(package)
         import("package.tools.xmake").install(package, { "-P", "core", "--pdcurses=n" })
